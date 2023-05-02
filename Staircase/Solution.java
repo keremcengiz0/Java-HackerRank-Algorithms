ebb550010d118a5214ff1e17448805249edc27a8
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-02 15:26:33 +0300

    added staircase

---
+++ Staircase/Solution.java
@@ -0,0 +1,35 @@
+package Staircase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Result {
+
+	public static void staircase(int n) {
+		
+		for (int i = 1; i <= n; i++) { 
+			for (int j = 1; j <= n - i; j++) { 
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) { 
+				System.out.print("#");
+			}
+			System.out.println(); 
+		}
+
+	}
+
+}
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+		Result.staircase(n);
+
+		bufferedReader.close();
+	}
+}
